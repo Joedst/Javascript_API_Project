6ef95abbb58feb0cd58ac98273550245280d1693
Joedst <joeledstrom@outlook.com>
2023-03-26 20:34:59 +0200

    Added buy button, general clean up, fakestoreAPI up again!

--- app.js
+++ app.js
@@ -7,10 +7,21 @@ const categoriesText = document.getElementById("categoriesText");
 const electronicsCategoryBtn = document.createElement("Button")
 
 
+const purchaseBtn = document.createElement("Button");
+purchaseBtn.innerHTML='Buy'
+purchaseBtn.addEventListener("click", function() {
+  buyingFunction(this.dataset.itemTitle);
+  
+});
+
+
+
 
 fetch('https://fakestoreapi.com/products/categories').then((data) =>{
 console.log(data)
 return data.json();
+
+
 })
 
 .then((objectData)=>{
@@ -22,47 +33,21 @@ objectData.map((values)=>{
 document.getElementById("category-header").innerHTML=tableData;
 
 
-
-
-
-
 })
 
 
+fetch('https://fakestoreapi.com/products/categories')
+            .then(res=>res.json())
+            .then(json=>console.log(json))
+
+//orders/vCCAsefKZ65ciE1MNTD8
 
 
+function buyingFunction($itemTitle){
+  console.log('Object ${itemTitle} was added to purchase');
+  //skicka till annan sida
 
-
-
-
-
-//objectData.addEventListener("click", myScript);
-
-
-
-
-
-/*
-fetch('https://fakestoreapi.com/products/').then((data) =>{
-console.log(data)
-return data.json();
-})
-
-.then((objectData)=>{
-  console.log(objectData[0] + " This is object number one");
-let tableData="";
-objectData.map((itemValues)=>{
-  tableData+=`${itemValues.title} 
-  <img src="${itemValues.image}"/>  <br>`
-
-  
-  
-  ;
-});
-document.getElementById("items-body").innerHTML=tableData;
-
-
-}) */
+}
 
 
 
@@ -79,34 +64,13 @@ objectData.map((itemValues)=>{
   tableData+=`<h1>${itemValues.title}</h1>
   <img src="${itemValues.image}"/>  
   <b>${itemValues.price} USD </b> <br>
-  <b>${itemValues.description}
-  `;
- 
-  
+  <b>${itemValues.description} <br>`;
+  purchaseBtn.dataset.itemTitle = itemValues.title;
+ tableData += purchaseBtn.outerHTML; 
 });
 document.getElementById("items-body").
 innerHTML=tableData;
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 if(Response){
@@ -114,93 +78,4 @@ if(Response){
 } else {
   console.log('Connection error')
 }
-    
-
-/*
-function printJSon(output){
-  (console.log(output.categories[i] + "test"));
-  output.categoriesEl.innerHTML = output.categoriesEl2.title;
-
-for (let i = 0; i < output.categories.length; i++){
-  title.innerHTML = output.categories[i];
-
-
-}
-
-
-
-
-} */
-
-
-
-
-/*
-fetch('https://fakestoreapi.com/products?sort=desc')
-            .then(res=>res.json())
-            .then(json=>console.log(json))
-
-*/
-
-/*
-function printJSon(output){
-  (console.log(output.programs[17].email));
-  email.innerHTML = output.programs[17].email;
-
-
-
-  for (let i = 0; i < output.category.length; i++) {
-    progImageEl.innerHTML = output.programs[i].programimage;
-
-    textEl.innerHTML += "<li>" + output.programs[i].name + "<br>" + output.programs[i].description + "</li>" +  
-     "<img height=200px src=" + output.programs[i].programimage + ">" + "<br>";
-    
-console.log(output.programs[i].name)
-       .then(Response => Response.json())
-  .then(json=>console.log(json))
-
-
-*/
-
-
-
-
-/*
-function printJSon(output){
-  (console.log(output.programs[17].email));
-  email.innerHTML = output.programs[17].email;
-
-
-
-  for (let i = 0; i < output.category.length; i++) {
-    progImageEl.innerHTML = output.programs[i].programimage;
-
-    textEl.innerHTML += "<li>" + output.programs[i].name + "<br>" + output.programs[i].description + "</li>" +  
-     "<img height=200px src=" + output.programs[i].programimage + ">" + "<br>";
-    
-console.log(output.programs[i].name)
-    
-
-
-
-
-
-if(Response.fetch){
-  console.log('Successfully connected!')
-} else {
-  console.log('Connection error')
-}
-    
-
-function printJSon(output){
-  (console.log(output.categories[i] + "test"));
-  output.categoriesEl.innerHTML = output.categoriesEl2.title;
-
-for (let i = 0; i < output.categories.length; i++){
-  title.innerHTML = output.categories[i];
-
-
-}
-
-
-*/
+  
