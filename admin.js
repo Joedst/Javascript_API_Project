06ffafe212aa4cbc3038774b8bb71bfc260af760
Joedst <joeledstrom@outlook.com>
2023-08-31 21:46:05 +0200

    Project rehaul, build of admin page

--- admin.js
+++ admin.js
@@ -1,23 +1,153 @@
 import { db } from './firebase-db.js';
+"use strict";
 
 
-
-
-const nameEl = document.getElementById("admin-name");
-const addressEl = document.getElementById("admin-address");
+const orderId = document.getElementById("admin-orderId")
+const fullNameEl = document.getElementById("admin-name");
 const emailEl = document.getElementById("admin-email");
-const idEl = document.getElementById("admin-id")
+const addressEl = document.getElementById("admin-address");
+const productIdEl = document.getElementById("admin-productId")
 const shippingEl = document.getElementById("admin-shipping");
 const adminSectionEl = document.getElementById("adminSection");
 
 
-fetch("https://firestore.googleapis.com/v1/projects/JavascriptAPIDatabase/databases/(default)/documents/orders/") //rätt nu men får 403
+fetch("https://firestore.googleapis.com/v1/projects/new-javascript-api-4d376/databases/(default)/documents/orders") //rätt nu men får 403
 .then(res => res.json())
 .then(data => getAdminContent(data))
 .catch(error => console.log(error));
 
-function getAdminContent(db)
-{
+
+
+
+
+
+function updateOrderData(name){
+    const orderId = idEl.value;
+    const customerName = fullNameEl.value;
+    const customerEmail = emailEl.value;
+    const customerAddress = addressEl.value;
+    const shippingMethod = shippingEl.value;
+
+let body = JSON.stringify(
+
+
+    {
+        "fields": {
+
+            "idproduct": {
+                "arrayValue": {
+                    "values": orderId.split(",").map(id => ({"stringValue": id.trim()}))
+                },
+
+            "Name": { 
+                "stringValue": customerName
+            },
+            "email": {
+                "stringValue": customerEmail
+            },
+            "address": {
+                "stringValue": customerAddress
+            },
+            "shipping": {
+                "stringValue": shippingMethod
+            }
+            
+            }
+        } 
+    } 
+
+
+)
+console.log("Updating function active.... Beginning fetch");
+
+fetch("https://firestore.googleapis.com/v1/" + name, {
+  
+method: "PATCH",
+headers: {
+
+  "Content-type": "application/json"
+
+},
+
+body: body
+
+})
+.then(res => res.json())
+.then(data => console.log(data))
+.catch(error => console.log(error));
+console.log(body)
+
+console.log("Fetch complete! user updated")
+
 
 }
 
+
+function getAdminContent(content){
+
+    const contDocs = content.documents;
+    let documentOrderID = '';
+
+    if (content.name) {
+      const fullDocsPath = content.name;
+      
+      const parts = fullDocsPath.split('/');
+      documentOrderID = parts[parts.length - 1];
+      
+      console.log("Extracted orderID:", documentOrderID);
+    } else {
+      console.log("Content name is undefined.");
+    }
+
+for(let content of contDocs) {
+
+    console.log(documentOrderID);
+console.log(content);
+
+adminSectionEl.innerHTML += `
+        <br>
+        <article class="articleFrame">
+            <ul class="admin">
+                <li> Order Link ID: ${content.name}</li>
+                <br>
+                <li>Customer Name: ${content.fields.fullName.stringValue}</li>
+                <li>Customer Adress: ${content.fields.adress.stringValue}</li>
+                <li>Customer Email: ${content.fields.email.stringValue}</li>
+                <li>Product ordered: ${content.fields.productId.stringValue}</li>
+
+                
+                
+            </ul>
+            <hr>
+                
+                <input type="button" value="update order" onClick="update('${content.name}')"> 
+        </article>
+        `
+
+
+}
+
+}
+
+//<li>Customer Shipping: ${content.fields.shipping.stringValue}</li> <li>Product IDs: ${(content.fields.productId.arrayValue.values.map(values => values.stringValue))}</li>
+
+/*
+constOrdersCollection = db.collectionGroup('orders')
+const orderId = content.orderId;
+
+
+ordersCollection.where(admin.firestore.FieldPath.documentId(), "==", orderId)
+  .get()
+  .then((querySnapshot) => {
+    if (!querySnapshot.empty) {
+      const orderDocRef = querySnapshot.docs[0].ref;
+
+    } else {
+        console.log("Order not found");
+      }
+    })
+    .catch((error) => {
+      console.error("Error retrieving order: ", error);
+    });
+<input type="button" value="delete order" onClick="deleteUser('${content.name}')">
+*/
--- new firebase-db.js
+++ new firebase-db.js
@@ -1,1 +1,32 @@
-https://firestore.googleapis.com/v1/projects/new-javascript-api-4d376/databases(default)/documents/orders
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://support.google.com/firebase/answer/7015592
+const firebaseConfig = {
+    apiKey: "AIzaSyBpseFAaok0MYKmsW_b0FzBVebU9-xHpoY",
+  authDomain: "new-javascript-api-4d376.firebaseapp.com",
+  projectId: "new-javascript-api-4d376",
+  storageBucket: "new-javascript-api-4d376.appspot.com",
+  messagingSenderId: "74720463257",
+  appId: "1:74720463257:web:7213ee6034b393792c6b08"
+};
+
+
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+
+
+
+
+
+
+
+//https://firestore.googleapis.com/v1/projects/new-javascript-api-4d376/databases/(default)/documents/orders
