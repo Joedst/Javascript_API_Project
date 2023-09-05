1e3f220283486179f9a686d903103394ccb01855
Joedst <joeledstrom@outlook.com>
2023-09-05 20:14:58 +0200

    Update function fixed

--- admin.js
+++ admin.js
@@ -17,116 +17,104 @@ fetch("https://firestore.googleapis.com/v1/projects/new-javascript-api-4d376/dat
 .then(data => getAdminContent(data))
 .catch(error => console.log(error));
 
+let documentOrderID = '';
+
 
 
 
 orderUpdateButtonEl.addEventListener('click', () => {
     console.log("Update order was clicked"); 
-    updateOrderData('https://firestore.googleapis.com/v1/projects/new-javascript-api-4d376/databases/(default)/documents/orders')
+    updateOrderData();
 });
 
-
-function updateOrderData(id){
-    const customerId = orderId.value;
+function updateOrderData() {
+    const documentId = orderId.value; 
     const customerName = fullNameEl.value;
     const customerEmail = emailEl.value;
     const customerAddress = addressEl.value;
-    //const shippingMethod = shippingEl.value;
     const productID = productIdEl.value;
-    
-    
-    orderUpdateButtonEl.addEventListener('click', () => {
-        let body = JSON.stringify({
-            "fields": {
-                
-                "fullName": { 
-                    "stringValue": customerName
-                },
-                "email": {
-                    "stringValue": customerEmail
-                },
-                "adress": {
-                    "stringValue": customerAddress
-                },
-                
-                "productId": {
-                    "stringValue": productID
-                }
-            }
-        });
+    const selectedShippingOption = shippingEl.value;
 
-        console.log("Updating function active.... Beginning fetch");
+    console.log("This is documentiD" + documentId);
+    
 
-        fetch("https://firestore.googleapis.com/v1/" + id, {
-            method: "PATCH",
-            headers: {
-                "Content-type": "application/json"
+    let body = JSON.stringify({
+        "fields": {
+            "fullName": {
+                "stringValue": customerName
             },
-            body: body
-        })
-        .then(res => res.json())
-        .then(data => console.log(data))
-        .catch(error => console.log(error));
-
-        console.log(body);
-        console.log("Fetch complete! order updated");
-
-        
-    
+            "email": {
+                "stringValue": customerEmail
+            },
+            "address": {
+                "stringValue": customerAddress
+            },
+            "productId": {
+                "stringValue": productID
+            },
+            "shipping": {
+                "stringValue": selectedShippingOption
+            }
+        }
     });
+    console.log("This is documentid " + documentId);
+    console.log("Updating function active.... Beginning fetch");
+
+    // Construct the Firestore document URL with the retrieved documentId
+    const firestoreDocumentURL = `https://firestore.googleapis.com/v1/projects/new-javascript-api-4d376/databases/(default)/documents/orders/${documentId}`;
+
+    fetch(firestoreDocumentURL, {
+        method: "PATCH", // Use PATCH to update an existing document
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: body
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        console.log("Fetch complete! order updated");
+    })
+    .catch(error => console.log(error));
 }
-
-
-function getAdminContent(content){
-
+function getAdminContent(content) {
     const contDocs = content.documents;
-    let documentOrderID = '';
 
     if (content.name) {
-      const fullDocsPath = content.name;
-      
-      const parts = fullDocsPath.split('/');
-      documentOrderID = parts[parts.length - 1];
-      
-      console.log("Extracted orderID:", documentOrderID);
+        const fullDocsPath = content.name;
+        const parts = fullDocsPath.split('/');
+        const documentOrderID = parts[parts.length - 1]; 
+        console.log("Extracted orderID:", documentOrderID);
+
+        let adminHTML = ""; // Initialize an empty string
+
+        for (let content of contDocs) {
+            console.log(documentOrderID);
+            console.log(content);
+
+            adminHTML += `
+                <br>
+                <article class="articleFrame">
+                    <ul class="admin">
+                        <li> Order ID: ${documentOrderID}</li>
+                        <br>
+                        <li>Customer Name: ${content.fields.fullName.stringValue}</li>
+                        <li>Customer Address: ${content.fields.address.stringValue}</li>
+                        <li>Customer Email: ${content.fields.email.stringValue}</li>
+                        <li>Product id: ${content.fields.productId.stringValue}</li>
+                        <li>Shipping: ${content.fields.shipping.stringValue}</li>
+                        <br>
+                        <input type="button" value="Delete order" onClick="deleteOrder('${content.name}')">
+                    </ul>
+                    <hr>
+                </article>
+            `;
+        }
+
+        adminSectionEl.innerHTML = adminHTML; // Set the innerHTML after the loop
     } else {
-      console.log("Content name is undefined.");
+        console.log("Content name is undefined.");
     }
-
-for(let content of contDocs) {
-
-    console.log(documentOrderID);
-console.log(content);
-
-adminSectionEl.innerHTML += `
-        <br>
-        <article class="articleFrame">
-            <ul class="admin">
-                <li> Order Link ID: ${content.name}</li>
-                <br>
-                <li>Customer Name: ${content.fields.fullName.stringValue}</li>
-                <li>Customer Address: ${content.fields.adress.stringValue}</li>
-                <li>Customer Email: ${content.fields.email.stringValue}</li>
-                <li>Product id: ${content.fields.productId.stringValue}</li>
-                <li>Shipping: ${content.fields.shipping.stringValue}</li>
-                
-                
-                
-                
-
-                <br>
-                <input type="button" value="Delete order" onClick="deleteOrder('${content.name}')">
-                
-            </ul>
-            <hr>
-                
-            
-        </article>
-        `
-
-
-}
-
 }
 
 //<li>Customer Shipping: ${content.fields.shipping.stringValue}</li> <li>Product IDs: ${(content.fields.productId.arrayValue.values.map(values => values.stringValue))}</li>
